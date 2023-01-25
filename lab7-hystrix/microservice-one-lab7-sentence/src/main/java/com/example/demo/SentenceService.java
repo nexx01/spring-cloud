a9f7dc10b05befd0d3f115e7ccc6be05fb7cdf61
Алексей Ашутов <i.ashutov@yandex.ru>
2023-01-25 21:37:11 +0300

    lab9 with gateway

--- lab7-hystrix/microservice-one-lab7-sentence/src/main/java/com/example/demo/SentenceService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-public interface SentenceService {
-
-    public String buildSentence();
-
-}
--- lab8-spring-bus/lucky-word-service-lab8/src/main/java/com/example/demo/LuckyWordApplicationLab8.java
+++ lab8-spring-bus/lucky-word-service-lab8/src/main/java/com/example/demo/LuckyWordApplicationLab8.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
+
 public class LuckyWordApplicationLab8 {
 
 	public static void main(String[] args) {
